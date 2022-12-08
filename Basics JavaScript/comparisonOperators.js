6b1fa0a4a59fccfc9128d2698303ae2e5e730f42
nick.kontsevych <nick.kontsevych@gmail.com>
2022-12-08 12:39:03 +0200

    Basic compare operations >, <=, !=

---
+++ Basics JavaScript/comparisonOperators.js
@@ -0,0 +1,11 @@
+console.log("Comparison operators")
+let numberA = 10;
+let numberB = 100;
+let numberC = numberA;
+console.log("numberA > numberB = ", numberA > numberB); //false
+console.log("numberA < numberB = ", numberA < numberB); //true
+console.log("numberA >= numberC = ", numberA >= numberC); //true
+console.log("numberA == numberB = ", numberA == numberB); //false
+console.log("numberA == numberC = ", numberA == numberC); //true
+console.log("numberA != numberB = ", numberA != numberB); //true
+console.log("numberA != numberC = ",  numberA != numberC); //false
