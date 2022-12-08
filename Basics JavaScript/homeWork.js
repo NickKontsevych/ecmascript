fde8d881f1f4a9ad9403b028b429c42a28b526ad
nick.kontsevych <nick.kontsevych@gmail.com>
2022-12-08 20:58:27 +0200

    Home work

---
+++ Basics JavaScript/homeWork.js
@@ -0,0 +1,37 @@
+/**
+ * Will be alert shown?
+ * if('0'){
+ *     alert('Hey');
+ * }
+ * Yes, the alert message will be shown. The string '0' == true
+ */
+
+if (prompt("What is real name of JS?") === "ECMAScript") {
+    alert("Correct!");
+} else {
+    alert("You don't know, ECMAScript!");
+}
+
+let userEnter = +prompt("Enter any number:")
+if (userEnter >=1) {
+    alert(1);
+} else if (userEnter === 0) {
+    alert(0);
+} else {
+    alert(-1);
+}
+
+let result = a + b < 4 ? "Lower" : "Upper";
+
+let message;
+if (login == 'Employee') {
+    message = 'Привіт';
+} else if (login == 'Boss') {
+    message = 'Hey';
+} else if (login == '') {
+    message = 'ninja';
+} else {
+    message = '';
+}
+
+let sendMessage = login == 'Employee'? "Hello": login === 'Boss'? "Hey": login === ''? "ninja": '';
